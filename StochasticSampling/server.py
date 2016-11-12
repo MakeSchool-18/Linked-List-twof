dcfe2f77a3c6e973ef32f437ca5eef50c89eeb58
twof <fabiobean2@gmail.com>
2016-11-11 16:05:19 -0800

    now takes query for sentence len

--- StochasticSampling/server.py
+++ StochasticSampling/server.py
@@ -10,7 +10,9 @@ app = Flask(__name__)
 def get_rand_word():
     histogram = gen_histogram_dict(open_doc("beeMovie.txt"))
     sentence = []
-    for i in range(10):
+    sentence_len = int(request.args.get('q'))
+
+    for i in range(sentence_len):
         sentence.append(rand_word(histogram, get_total(histogram)))
     return " ".join(sentence)
 
